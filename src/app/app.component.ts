af8e1857a19fb7d16b235e9c26dfb101e345b5e0
Shazeen Fabius <shazzy.sterlin@gmail.com>
2023-02-10 20:06:23 -0800

    ng directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@
 // export class AppComponent {os/id/237/500/500.jpg?hmac
 //   title = 'basics';
 // }
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -19,19 +18,36 @@ import { Component } from "@angular/core";
   // styles:
 })
 export class AppComponent {
-  name = "Shazeen"
-  imgUrl = "https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0"
+  name = 'shazeen';
+  imgUrl =
+    'https://media.os.fressnapf.com/cms/2020/07/ratgeber_hund_rasse_portraits_shih-tzu_1200x527.jpg?t=seoimg_937';
 
-  getName(){
-    return this.name
+  images = [
+    'https://www.petful.com/wp-content/uploads/2012/03/shih-tzu.jpg',
+    'https://www.allthingsdogs.com/wp-content/uploads/2021/12/cute-shih-tzu.jpg',
+    'https://s36700.pcdn.co/wp-content/uploads/2017/04/iStock-shihtzu_thumb-600x400.jpg.optimal.jpg',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pineapple', 'mushroom'],
+    size: 'large',
+  };
+
+  blueClass = false;
+  fontSize = 16;
+
+  getName() {
+    return this.name;
   }
 
   changeImage(e: KeyboardEvent) {
-    this.imgUrl = (e.target as HTMLInputElement).value
+    this.imgUrl = (e.target as HTMLInputElement).value;
   }
 
   logImg(event: string) {
-    console.log(event)
-    console.log("log img method is working")
+    console.log(event);
+    console.log('log img method is working');
   }
 }
